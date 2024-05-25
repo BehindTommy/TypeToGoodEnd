c98c6d016ca732e75f79ae42b241147cef33ac6d
BehindTommy <aiyi8200@outlook.com>
2024-05-25 14:12:17 +0800

    first commit

---
+++ main.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public partial class main : Node2D
+{
+	private AnimatedSprite2D MC_sprite;
+
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		MC_sprite = GetNode<AnimatedSprite2D>("main_character");
+		MC_sprite.Play("idle");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if(delta > 1)
+		{
+			MC_sprite.Play("run");
+		}
+	}
+}
